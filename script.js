dc24a4e54359d54f219a8b8118ce43d5ddc7d698
ITzRAHULLL <rahul.d.gowda3@gmail.com>
2025-07-22 20:56:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const boing = document.getElementById("boingSound");
+
+let clickCount = 0;
+
+noBtn.addEventListener("mouseover", () => {
+  clickCount++;
+  const maxX = window.innerWidth - noBtn.offsetWidth;
+  const maxY = window.innerHeight - noBtn.offsetHeight;
+
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
+
+  noBtn.style.position = "absolute";
+  noBtn.style.left = `${randomX}px`;
+  noBtn.style.top = `${randomY}px`;
+
+  noBtn.style.transform = `scale(${1 - clickCount * 0.05})`;
+  boing.play();
+});
+
+yesBtn.addEventListener("click", () => {
+  document.querySelector(".content").style.opacity = "0";
+  setTimeout(() => {
+    window.location.href = "thankyou.html";
+  }, 800);
+});
